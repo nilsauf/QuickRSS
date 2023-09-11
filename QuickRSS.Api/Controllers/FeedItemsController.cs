20bb110589ffb1666585042e70b9a39e81f18e8d
Nils Aufschläger <mail@nilsauf.de>
2023-09-11 20:26:22 +0200

    adding FeedItemsController to get feed items

---
+++ QuickRSS.Api/Controllers/FeedItemsController.cs
@@ -0,0 +1,45 @@
+﻿namespace QuickRSS.Api.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using QuickRSS.Database.Feed;
+    using QuickRSS.Entities;
+
+    [Route("api/feed/items")]
+    [ApiController]
+    [Authorize]
+    public class FeedItemsController : ControllerBase
+    {
+        private readonly IFeedStore feedStore;
+
+        public FeedItemsController(IFeedStore feedStore)
+        {
+            this.feedStore = feedStore;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(Guid feed, Guid? item)
+        {
+            var feedEntity = await feedStore.GetAsync(feed);
+            if (feedEntity is null)
+            {
+                return NotFound();
+            }
+
+            return item.HasValue ?
+                GetItem(feedEntity, item.Value.ToString()) :
+                GetAllItems(feedEntity);
+
+            IActionResult GetAllItems(Feed feed) => Ok(feed.Items);
+
+            IActionResult GetItem(Feed feed, string itemId)
+            {
+                var feedItem = feed.Items.FirstOrDefault(item => item.Id == itemId);
+                if (feedItem is null)
+                    return NotFound();
+
+                return Ok(feedItem);
+            }
+        }
+    }
+}
--- QuickRSS.Entities/FeedItem.cs
+++ QuickRSS.Entities/FeedItem.cs
@@ -6,6 +6,6 @@
         public string Title { get; init; }
         public string Content { get; init; }
         public string URL { get; init; }
-        public string PublishingDate { get; set; }
+        public string PublishingDate { get; init; }
     }
 }
