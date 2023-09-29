84d8952efd1bc08e79e75aa719195cf3b1ba10f8
Nils Aufschläger <mail@nilsauf.de>
2023-09-29 20:52:41 +0200

    adding scheduler as dependency

--- QuickRSS.Logic/FeedAggregator.cs
+++ QuickRSS.Logic/FeedAggregator.cs
@@ -3,6 +3,7 @@
     using DynamicData;
     using QuickRSS.Entities;
     using System;
+    using System.Reactive.Concurrency;
     using System.Reactive.Linq;
     using System.Reactive.Threading.Tasks;
 
@@ -11,15 +12,18 @@
         private readonly IFeedLoader feedLoader;
         private readonly IFeedProvider feedProvider;
         private readonly IFeedUpdater feedUpdater;
+        private readonly IScheduler scheduler;
 
         public FeedAggregator(
             IFeedLoader feedLoader,
             IFeedProvider feedProvider,
-            IFeedUpdater feedUpdater)
+            IFeedUpdater feedUpdater,
+            IScheduler? scheduler = null)
         {
             this.feedLoader = feedLoader ?? throw new ArgumentNullException(nameof(feedLoader));
             this.feedProvider = feedProvider ?? throw new ArgumentNullException(nameof(feedProvider));
             this.feedUpdater = feedUpdater ?? throw new ArgumentNullException(nameof(feedUpdater));
+            this.scheduler = scheduler ?? Scheduler.Default;
         }
 
         public Task RunAsync(CancellationToken cancellationToken)
@@ -32,8 +36,9 @@
             IDisposable StartSingleFeedAggregation(Feed feed)
             {
                 return Observable.FromAsync(cancellationToken =>
-                        this.feedLoader.LoadAsync(feed.URL, cancellationToken))
-                    .DelaySubscription(feed.RefreshRate)
+                        this.feedLoader.LoadAsync(feed.URL, cancellationToken),
+                        this.scheduler)
+                    .DelaySubscription(feed.RefreshRate, this.scheduler)
                     .Do(fetchedFeed => fetchedFeed.Id = feed.Id)
                     .SelectMany(async fetchedFeed => await this.feedUpdater.UpdateFeedAsync(fetchedFeed))
                     .Repeat()
