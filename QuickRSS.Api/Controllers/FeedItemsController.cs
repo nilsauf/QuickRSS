5eb71ba30944455cb812aa1ba7e9c67f3a30604f
Nils Aufschläger <mail@nilsauf.de>
2023-10-12 21:15:19 +0200

    cleanup

--- QuickRSS.Api/Controllers/FeedItemsController.cs
+++ QuickRSS.Api/Controllers/FeedItemsController.cs
@@ -1,45 +1,42 @@
 ﻿namespace QuickRSS.Api.Controllers
 {
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
-    using QuickRSS.Database.Feed;
-    using QuickRSS.Entities;
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
+	using QuickRSS.Database.Feed;
+	using QuickRSS.Entities;
 
-    [Route("api/feed/items")]
-    [ApiController]
-    [Authorize]
-    public class FeedItemsController : ControllerBase
-    {
-        private readonly IFeedStore feedStore;
+	[Route("api/feed/items")]
+	[ApiController]
+	[Authorize]
+	public class FeedItemsController : ControllerBase
+	{
+		private readonly IFeedStore feedStore;
 
-        public FeedItemsController(IFeedStore feedStore)
-        {
-            this.feedStore = feedStore;
-        }
+		public FeedItemsController(IFeedStore feedStore)
+		{
+			this.feedStore = feedStore;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Get(Guid feed, Guid? item)
-        {
-            var feedEntity = await feedStore.GetAsync(feed, true);
-            if (feedEntity is null)
-            {
-                return NotFound();
-            }
+		[HttpGet]
+		public async Task<IActionResult> Get(Guid feed, string? item)
+		{
+			var feedEntity = await feedStore.GetAsync(feed, true);
+			if (feedEntity is null)
+			{
+				return NotFound();
+			}
 
-            return item.HasValue ?
-                GetItem(feedEntity, item.Value.ToString()) :
-                GetAllItems(feedEntity);
+			return string.IsNullOrWhiteSpace(item) ?
+				GetAllItems(feedEntity) :
+				GetItem(feedEntity, item);
 
-            IActionResult GetAllItems(Feed feed) => Ok(feed.Items);
+			IActionResult GetAllItems(Feed feed) => Ok(feed.Items);
 
-            IActionResult GetItem(Feed feed, string itemId)
-            {
-                var feedItem = feed.Items.FirstOrDefault(item => item.Id == itemId);
-                if (feedItem is null)
-                    return NotFound();
-
-                return Ok(feedItem);
-            }
-        }
-    }
+			IActionResult GetItem(Feed feed, string itemId)
+			{
+				var feedItem = feed.Items.FirstOrDefault(item => item.Id == itemId);
+				return feedItem is null ? NotFound() : Ok(feedItem);
+			}
+		}
+	}
 }
--- QuickRSS.Database/Feed/FeedStore.cs
+++ QuickRSS.Database/Feed/FeedStore.cs
@@ -1,107 +1,105 @@
 ﻿namespace QuickRSS.Database.Feed
 {
-    using Microsoft.Extensions.Logging;
-    using QuickRSS.Database;
-    using QuickRSS.Database.Extensions;
-    using QuickRSS.Database.User;
-    using QuickRSS.Entities;
-    using System;
-    using System.Collections.Generic;
+	using Microsoft.Extensions.Logging;
+	using QuickRSS.Database;
+	using QuickRSS.Database.Extensions;
+	using QuickRSS.Database.User;
+	using QuickRSS.Entities;
+	using System;
+	using System.Collections.Generic;
 
-    public class FeedStore : IFeedStore
-    {
-        private readonly ILogger<FeedStore> logger;
-        private readonly IQuickRssDataAccess dataAccess;
-        private readonly IFeedChangeNotifier changeNotifier;
-        private readonly ICurrentUserProvider userProvider;
-        private readonly IEqualityComparer<Guid> guidComparer;
+	public class FeedStore : IFeedStore
+	{
+		private readonly ILogger<FeedStore> logger;
+		private readonly IQuickRssDataAccess dataAccess;
+		private readonly IFeedChangeNotifier changeNotifier;
+		private readonly ICurrentUserProvider userProvider;
+		private readonly IEqualityComparer<Guid> guidComparer;
 
-        public FeedStore(
-            ILogger<FeedStore> logger,
-            IQuickRssDataAccess dataAccess,
-            IFeedChangeNotifier changeNotifier,
-            ICurrentUserProvider userProvider,
-            IEqualityComparer<Guid> guidComparer)
-        {
-            this.logger = logger;
-            this.dataAccess = dataAccess;
-            this.changeNotifier = changeNotifier;
-            this.userProvider = userProvider;
-            this.guidComparer = guidComparer;
-        }
+		public FeedStore(
+			ILogger<FeedStore> logger,
+			IQuickRssDataAccess dataAccess,
+			IFeedChangeNotifier changeNotifier,
+			ICurrentUserProvider userProvider,
+			IEqualityComparer<Guid> guidComparer)
+		{
+			this.logger = logger;
+			this.dataAccess = dataAccess;
+			this.changeNotifier = changeNotifier;
+			this.userProvider = userProvider;
+			this.guidComparer = guidComparer;
+		}
 
-        public async Task<bool> AddAsync(Feed feed)
-        {
-            var user = await this.userProvider.GetAsync();
-            if (user is null)
-                return false;
+		public async Task<bool> AddAsync(Feed feed)
+		{
+			var user = await this.userProvider.GetAsync();
+			if (user is null)
+				return false;
 
-            user.Feeds.Add(feed);
-            await this.dataAccess.AddAsync(feed);
-            await this.SaveAndNotifyChangesAsync(feed.Id);
-            return true;
-        }
+			user.Feeds.Add(feed);
+			await this.dataAccess.AddAsync(feed);
+			await this.SaveAndNotifyChangesAsync(feed.Id);
+			return true;
+		}
 
-        public async Task<bool> RemoveAsync(Guid id)
-        {
-            var user = await this.userProvider.GetAsync();
-            if (user is null)
-                return false;
+		public async Task<bool> RemoveAsync(Guid id)
+		{
+			var user = await this.userProvider.GetAsync();
+			if (user is null)
+				return false;
 
-            var feed = user.Feeds.FirstOrDefault(feed => guidComparer.Equals(feed.Id, id));
-            if (feed is null)
-                return false;
+			var feed = user.Feeds.FirstOrDefault(feed => guidComparer.Equals(feed.Id, id));
+			if (feed is null)
+				return false;
 
-            user.Feeds.Remove(feed);
-            this.dataAccess.Remove(feed);
-            await this.SaveAndNotifyChangesAsync(id);
-            return true;
-        }
+			user.Feeds.Remove(feed);
+			this.dataAccess.Remove(feed);
+			await this.SaveAndNotifyChangesAsync(id);
+			return true;
+		}
 
-        public async Task<Feed?> GetAsync(
-            Guid id,
-            bool includeFeedItems = false,
-            CancellationToken cancellationToken = default)
-        {
-            var user = await this.userProvider.GetAsync(includeFeedItems, cancellationToken);
-            if (user is null)
-                return null;
+		public async Task<Feed?> GetAsync(
+			Guid id,
+			bool includeFeedItems = false,
+			CancellationToken cancellationToken = default)
+		{
+			var user = await this.userProvider.GetAsync(includeFeedItems, cancellationToken);
+			if (user is null)
+				return null;
 
-            return user.Feeds.FirstOrDefault(feed => guidComparer.Equals(feed.Id, id));
-        }
+			return user.Feeds.FirstOrDefault(feed => this.guidComparer.Equals(feed.Id, id));
+		}
 
-        public async Task<IEnumerable<Feed>?> GetAllAsync(
-            bool includeFeedItems = false,
-            CancellationToken cancellationToken = default)
-        {
-            var user = await this.userProvider.GetAsync(includeFeedItems, cancellationToken);
-            return user is null ?
-                null :
-                user.Feeds.ToList();
-        }
+		public async Task<IEnumerable<Feed>?> GetAllAsync(
+			bool includeFeedItems = false,
+			CancellationToken cancellationToken = default)
+		{
+			var user = await this.userProvider.GetAsync(includeFeedItems, cancellationToken);
+			return user?.Feeds.ToList();
+		}
 
-        public async Task<bool> UpdateAsync(
-            Guid id,
-            Action<Feed> update,
-            bool includeFeedItems = false)
-        {
-            if (update is null)
-                throw new ArgumentNullException(nameof(update));
+		public async Task<bool> UpdateAsync(
+			Guid id,
+			Action<Feed> update,
+			bool includeFeedItems = false)
+		{
+			if (update is null)
+				throw new ArgumentNullException(nameof(update));
 
-            var feed = await this.GetAsync(id, includeFeedItems);
-            if (feed is null)
-                return false;
+			var feed = await this.GetAsync(id, includeFeedItems);
+			if (feed is null)
+				return false;
 
-            update(feed);
+			update(feed);
 
-            await this.SaveAndNotifyChangesAsync(id);
-            return true;
-        }
+			await this.SaveAndNotifyChangesAsync(id);
+			return true;
+		}
 
-        private async Task SaveAndNotifyChangesAsync(Guid id, CancellationToken token = default)
-        {
-            await this.dataAccess.SaveChangesAsync(token);
-            this.changeNotifier.Notify(id);
-        }
-    }
+		private async Task SaveAndNotifyChangesAsync(Guid id, CancellationToken token = default)
+		{
+			await this.dataAccess.SaveChangesAsync(token);
+			this.changeNotifier.Notify(id);
+		}
+	}
 }
