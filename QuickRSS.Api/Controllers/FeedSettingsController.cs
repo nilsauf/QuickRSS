02bfddb14832ae41d853da8ec6c17a6de8c58ee8
Nils Aufschläger <mail@nilsauf.de>
2023-10-12 20:52:39 +0200

    cleanup

--- QuickRSS.Api/Controllers/FeedSettingsController.cs
+++ QuickRSS.Api/Controllers/FeedSettingsController.cs
@@ -1,80 +1,80 @@
 ﻿namespace QuickRSS.Api.Controllers
 {
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
-    using QuickRSS.Api.Extensions;
-    using QuickRSS.Api.Models;
-    using QuickRSS.Database.Feed;
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
+	using QuickRSS.Api.Extensions;
+	using QuickRSS.Api.Models;
+	using QuickRSS.Database.Feed;
 
-    [Route("api/feed/settings")]
-    [ApiController]
-    [Authorize]
-    public class FeedSettingsController : ControllerBase
-    {
-        private readonly IFeedStore feedStore;
+	[Route("api/feed/settings")]
+	[ApiController]
+	[Authorize]
+	public class FeedSettingsController : ControllerBase
+	{
+		private readonly IFeedStore feedStore;
 
-        public FeedSettingsController(IFeedStore feedStore)
-        {
-            this.feedStore = feedStore;
-        }
+		public FeedSettingsController(IFeedStore feedStore)
+		{
+			this.feedStore = feedStore;
+		}
 
-        [HttpGet()]
-        public Task<IActionResult> Get(Guid? id)
-        {
-            return id is Guid notNullId ?
-                GetById(notNullId) :
-                GetAll();
+		[HttpGet]
+		public Task<IActionResult> Get(Guid? id)
+		{
+			return id is Guid notNullId ?
+				GetById(notNullId) :
+				GetAll();
 
-            async Task<IActionResult> GetById(Guid id)
-            {
-                var feed = await this.feedStore.GetAsync(notNullId);
-                return feed is null ?
-                    StatusCode(StatusCodes.Status500InternalServerError) :
-                    Ok(feed.ToSettingsModel());
-            }
+			async Task<IActionResult> GetById(Guid id)
+			{
+				var feed = await this.feedStore.GetAsync(id);
+				return feed is null ?
+					StatusCode(StatusCodes.Status500InternalServerError) :
+					Ok(feed.ToSettingsModel());
+			}
 
-            async Task<IActionResult> GetAll()
-            {
-                var feeds = await this.feedStore.GetAllAsync();
-                return feeds is null ?
-                    StatusCode(StatusCodes.Status500InternalServerError) :
-                    Ok(feeds.Select(FeedExtensions.ToSettingsModel));
-            }
-        }
+			async Task<IActionResult> GetAll()
+			{
+				var feeds = await this.feedStore.GetAllAsync();
+				return feeds is null ?
+					StatusCode(StatusCodes.Status500InternalServerError) :
+					Ok(feeds.Select(FeedExtensions.ToSettingsModel));
+			}
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] FeedSettingsModel value)
-        {
-            var result = await this.feedStore.AddAsync(value.ToEntity());
+		[HttpPost]
+		public async Task<IActionResult> Post([FromBody] FeedSettingsModel value)
+		{
+			var result = await this.feedStore.AddAsync(value.ToEntity());
 
-            return result ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
-        }
+			return result ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
+		}
 
-        [HttpPut()]
-        public async Task<IActionResult> Put([FromBody] FeedSettingsModel value)
-        {
-            if (value.Id is null)
-            {
-                return StatusCode(StatusCodes.Status400BadRequest, new { error = "No id given!" });
-            }
+		[HttpPut]
+		public async Task<IActionResult> Put([FromBody] FeedSettingsModel value)
+		{
+			if (value.Id is null)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest, new { error = "No id given!" });
+			}
 
-            var newValues = value.ToEntity();
-            var result = await this.feedStore.UpdateAsync(newValues.Id, feed =>
-            {
-                feed.Title = newValues.Title;
-                feed.URL = newValues.URL;
-                feed.RefreshRate = newValues.RefreshRate;
-                feed.MaxItemCount = newValues.MaxItemCount;
-            });
+			var newValues = value.ToEntity();
+			var result = await this.feedStore.UpdateAsync(newValues.Id, feed =>
+			{
+				feed.Title = newValues.Title;
+				feed.URL = newValues.URL;
+				feed.RefreshRate = newValues.RefreshRate;
+				feed.MaxItemCount = newValues.MaxItemCount;
+			});
 
-            return result ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
-        }
+			return result ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
+		}
 
-        [HttpDelete()]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var result = await this.feedStore.RemoveAsync(id);
-            return result ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
-        }
-    }
+		[HttpDelete]
+		public async Task<IActionResult> Delete(Guid id)
+		{
+			var result = await this.feedStore.RemoveAsync(id);
+			return result ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
+		}
+	}
 }
