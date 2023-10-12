d7b39bcdb68a316e6f5251bba4128b0c7b905779
Nils Aufschläger <mail@nilsauf.de>
2023-10-12 21:50:33 +0200

    adding FeedItemStore

---
+++ QuickRSS.Database/FeedItem/FeedItemStore.cs
@@ -0,0 +1,65 @@
+﻿namespace QuickRSS.Database.FeedItem
+{
+	using Microsoft.Extensions.Logging;
+	using QuickRSS.Database.Feed;
+	using QuickRSS.Entities;
+	using System;
+	using System.Linq;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	public class FeedItemStore : IFeedItemStore
+	{
+		private readonly ILogger<FeedItemStore> logger;
+		private readonly IFeedStore feedStore;
+		private readonly IQuickRssDataAccess dataAccess;
+
+		public FeedItemStore(
+			ILogger<FeedItemStore> logger,
+			IFeedStore feedStore,
+			IQuickRssDataAccess dataAccess)
+		{
+			this.logger = logger;
+			this.feedStore = feedStore;
+			this.dataAccess = dataAccess;
+		}
+
+		public async Task<FeedItem?> GetAsync(string id, CancellationToken cancellationToken = default)
+		{
+			if (string.IsNullOrWhiteSpace(id))
+				throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
+
+			var feeds = await this.feedStore.GetAllAsync();
+			return feeds?.SelectMany(f => f.Items)
+				.FirstOrDefault(fi => fi.Id.Equals(id));
+		}
+
+		public async Task<bool> UpdateAsync(Action<FeedItem> update, params string[] ids)
+		{
+			if (ids.Length == 0) throw new InvalidOperationException("No id's to update specified!");
+			if (update is null) throw new ArgumentNullException(nameof(update));
+
+			var feeds = await this.feedStore.GetAllAsync();
+			if (feeds is null)
+			{
+				return false;
+			}
+
+			var feedItemsToUpdate = feeds.SelectMany(f => f.Items)
+				.Where(fi => ids.Contains(fi.Id))
+				.ToList();
+			if (feedItemsToUpdate.Count == 0)
+			{
+				return false;
+			}
+
+			foreach (var item in feedItemsToUpdate)
+			{
+				update(item);
+			}
+
+			await this.dataAccess.SaveChangesAsync();
+			return true;
+		}
+	}
+}
---
+++ QuickRSS.Database/FeedItem/IFeedItemStore.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickRSS.Database.FeedItem
+{
+	using QuickRSS.Entities;
+
+	public interface IFeedItemStore
+	{
+		Task<FeedItem?> GetAsync(string id, CancellationToken cancellationToken = default);
+		Task<bool> UpdateAsync(Action<FeedItem> update, params string[] ids);
+	}
+}
