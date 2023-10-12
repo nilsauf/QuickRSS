8c11368937c14dcf096c92002e8fde7eb63f9e90
Nils Aufschläger <mail@nilsauf.de>
2023-10-12 22:19:17 +0200

    even more cleanup today

--- QuickRSS.Api/Extensions/IServiceCollectionExtensions.cs
+++ QuickRSS.Api/Extensions/IServiceCollectionExtensions.cs
@@ -1,66 +1,77 @@
 ﻿namespace QuickRSS.Api.Extensions
 {
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.IdentityModel.Tokens;
-    using QuickRSS.Api.Services;
-    using QuickRSS.CodeHollowFeedReader;
-    using QuickRSS.Database;
-    using QuickRSS.Database.EFCore;
-    using QuickRSS.Database.Feed;
-    using QuickRSS.Database.User;
-    using QuickRSS.Logic;
-    using System.Text;
+	using Microsoft.AspNetCore.Authentication.JwtBearer;
+	using Microsoft.AspNetCore.Identity;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.IdentityModel.Tokens;
+	using QuickRSS.Api.Services;
+	using QuickRSS.CodeHollowFeedReader;
+	using QuickRSS.Database;
+	using QuickRSS.Database.EFCore;
+	using QuickRSS.Database.Feed;
+	using QuickRSS.Database.FeedItem;
+	using QuickRSS.Database.User;
+	using QuickRSS.Logic;
+	using System.Text;
 
-    public static class IServiceCollectionExtensions
-    {
-        public static IServiceCollection AddQuickRss(this IServiceCollection services, IConfiguration configuration)
-        {
-            string connectionString = configuration.GetConnectionString("DefaultConnection")
-                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            //return services.AddDbContext<QuickRssDbContext>(
-            //    options => options.UseSqlServer(connectionString));
+	public static class IServiceCollectionExtensions
+	{
+		public static IServiceCollection AddQuickRss(this IServiceCollection services, IConfiguration configuration)
+		{
+			string connectionString = configuration.GetConnectionString("DefaultConnection")
+				?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<QuickRssDbContext>()
-                .AddDefaultTokenProviders();
+			return services.AddTransient<FeedAggregator>()
+				.AddTransient<IFeedLoader, CodeHollowFeedLoader>()
+				.AddTransient<IFeedProvider, FeedProvider>()
+				.AddTransient<IFeedUpdater, FeedUpdater>()
+				.AddTransient<IFeedStore, FeedStore>()
+				.AddTransient<IFeedItemStore, FeedItemStore>()
+				.AddHostedService<FeedAggregatorService>()
+				.AddSingleton<IFeedChangeNotifier, FeedChangeNotifier>()
+				.AddDbContext<IQuickRssDataAccess, QuickRssDbContext>(
+					options => options.ConfigureForEFCore(connectionString))
+				.AddTransient<IQuickRssUserStore, QuickRssUserStore>()
+				.AddTransient<ICurrentUserProvider, CurrentUserProvider>()
+				.AddSingleton<IEqualityComparer<Guid>>(EqualityComparer<Guid>.Default)
+				.AddAuthentication(configuration)
+				.AddQuickRssIdentity();
+		}
 
-            // Adding Authentication
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = configuration["JWT:ValidAudience"],
-                    ValidIssuer = configuration["JWT:ValidIssuer"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]!))
-                };
-            });
+		public static IServiceCollection AddQuickRssIdentity(this IServiceCollection services)
+		{
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<QuickRssDbContext>()
+				.AddDefaultTokenProviders();
 
-            return services.AddTransient<FeedAggregator>()
-                .AddTransient<IFeedLoader, CodeHollowFeedLoader>()
-                .AddTransient<IFeedProvider, FeedProvider>()
-                .AddTransient<IFeedUpdater, FeedUpdater>()
-                .AddTransient<IFeedStore, FeedStore>()
-                .AddHostedService<FeedAggregatorService>()
-                .AddSingleton<IFeedChangeNotifier, FeedChangeNotifier>()
-                .AddDbContext<IQuickRssDataAccess, QuickRssDbContext>(
-                    options => options.ConfigureForEFCore(connectionString))
-                .AddTransient<IQuickRssUserStore, QuickRssUserStore>()
-                .AddTransient<ICurrentUserProvider, CurrentUserProvider>()
-                .AddSingleton<IEqualityComparer<Guid>>(EqualityComparer<Guid>.Default);
-        }
-    }
+			return services;
+		}
+
+		public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(options =>
+			{
+				options.SaveToken = true;
+				options.RequireHttpsMetadata = false;
+				options.TokenValidationParameters = new TokenValidationParameters()
+				{
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidAudience = configuration["JWT:ValidAudience"],
+					ValidIssuer = configuration["JWT:ValidIssuer"],
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]!))
+				};
+			});
+
+			return services;
+		}
+	}
 }
--- QuickRSS.Database.EFCore/DbContextOptionsBuilderExtensions.cs
+++ QuickRSS.Database.EFCore/DbContextOptionsBuilderExtensions.cs
@@ -1,21 +1,22 @@
 ﻿namespace QuickRSS.Database.EFCore
 {
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore.Diagnostics;
-    using System;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Diagnostics;
+	using System;
 
-    public static class DbContextOptionsBuilderExtensions
-    {
-        public static DbContextOptionsBuilder ConfigureForEFCore(this DbContextOptionsBuilder builder, string connectionString)
-        {
-            if (builder is null)
-                throw new ArgumentNullException(nameof(builder));
-            if (string.IsNullOrWhiteSpace(connectionString))
-                throw new ArgumentException($"'{nameof(connectionString)}' cannot be null or whitespace.", nameof(connectionString));
+	public static class DbContextOptionsBuilderExtensions
+	{
+		public static DbContextOptionsBuilder ConfigureForEFCore(this DbContextOptionsBuilder builder, string connectionString)
+		{
+			if (builder is null)
+				throw new ArgumentNullException(nameof(builder));
+			if (string.IsNullOrWhiteSpace(connectionString))
+				throw new ArgumentException($"'{nameof(connectionString)}' cannot be null or whitespace.", nameof(connectionString));
 
-            return builder
-                .UseInMemoryDatabase("DevDB")
-                .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
-        }
-    }
+			return builder
+				//.UseSqlServer(connectionString)
+				.UseInMemoryDatabase("DevDB")
+				.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+		}
+	}
 }
